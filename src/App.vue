e66a79ac738a5f978801d93864c4baf84ed5d233
Yohann Loïc Ravino <atticot.yohann@gmail.com>
2023-11-20 15:54:21 +0100

    src

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <HomePage />
+</template>
+
+<script>
+import HomePage from '@/views/HomePage';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/HeaderNavbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="p-6 bg-blue-500 text-white">
+    <nav>
+      <ul class="flex">
+        <li v-for="link in links" :key="link.name" class="mr-6">
+          <a :href="link.href" :class="{ 'font-bold': link.current }">{{ link.name }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderNavbar',
+  props: {
+    links: Array
+  }
+}
+</script>
+
+<style scoped>
+/* Styles spécifiques à HeaderNavbar ici */
+</style>
