b5970ac45a9c35988bb43605107fec12f802c3dc
Yohann Loïc Ravino <atticot.yohann@gmail.com>
2023-11-20 14:49:53 +0100

    exemple passing data

---
+++ ChildComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>{{ message }}</p>
+        <button @click="emitEvent">Envoyer un message au parent</button>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: ['message'],
+    methods: {
+        emitEvent() {
+            this.$emit('childEvent', 'Bonjour du composant enfant!');
+        }
+    }
+}
+</script>
+  
---
+++ ParentComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ChildComponent :message="parentMessage" @childEvent="handleChildEvent" />
+        <p>Message du composant enfant: {{ childMessage }}</p>
+    </div>
+</template>
+  
+<script>
+import ChildComponent from './ChildComponent.vue';
+
+export default {
+    components: {
+        ChildComponent
+    },
+    data() {
+        return {
+            parentMessage: 'Message du parent',
+            childMessage: ''
+        };
+    },
+    methods: {
+        handleChildEvent(data) {
+            this.childMessage = data;
+        }
+    }
+}
+</script>
